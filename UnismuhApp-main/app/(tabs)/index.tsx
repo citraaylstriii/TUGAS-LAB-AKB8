a53e23e32b66092a3402f94187b167bee5072aa3
citraaylstriii <139321959+citraaylstriii@users.noreply.github.com>
2025-08-12 03:07:14 +0800

    Update index.tsx

--- UnismuhApp-main/app/(tabs)/index.tsx
+++ UnismuhApp-main/app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Text, View, Image, ScrollView } from 'react-native';
-import { styles } from '../styles'; // Impor gaya dari file terpisah
+import { styles } from '../styles'; // Import styles from a separate file
 
 export default function HomeScreen() {
   return (
@@ -11,18 +11,25 @@ export default function HomeScreen() {
           style={styles.logo}
           resizeMode="contain"
         />
-        <Text style={styles.title}>Universitas Muhammadiyah Makassar</Text>
-        <Text style={styles.subtitle}>Kampus Biru, Pusat Pencerahan</Text>
+        <Text style={styles.title}>Muhammadiyah University of Makassar</Text>
+        <Text style={styles.subtitle}>Blue Campus, Center of Enlightenment</Text>
         <Text style={styles.description}>
-          Universitas Muhammadiyah (Unismuh) Makassar adalah salah satu perguruan tinggi swasta terkemuka di Indonesia Timur. Berlokasi di kota Makassar, Unismuh berkomitmen untuk menyelenggarakan pendidikan yang unggul, islami, dan mencerahkan.
+          Muhammadiyah University (Unismuh) Makassar is one of the leading private universities
+          in Eastern Indonesia. Located in the city of Makassar, Unismuh is committed to
+          delivering excellent, Islamic, and enlightening education.
         </Text>
         <View style={styles.infoBox}>
-          <Text style={styles.infoTitle}>Lokasi</Text>
-          <Text style={styles.infoText}>Jl. Sultan Alauddin No. 259, Makassar, Sulawesi Selatan, Indonesia.</Text>
+          <Text style={styles.infoTitle}>Location</Text>
+          <Text style={styles.infoText}>
+            Jl. Sultan Alauddin No. 259, Makassar, South Sulawesi, Indonesia.
+          </Text>
         </View>
-         <View style={styles.infoBox}>
-          <Text style={styles.infoTitle}>Visi</Text>
-          <Text style={styles.infoText}>Menjadi perguruan tinggi Islam terkemuka, unggul, terpercaya, dan mandiri pada tahun 2025.</Text>
+        <View style={styles.infoBox}>
+          <Text style={styles.infoTitle}>Vision</Text>
+          <Text style={styles.infoText}>
+            To become a leading, excellent, trustworthy, and independent Islamic higher education
+            institution by 2025.
+          </Text>
         </View>
       </View>
     </ScrollView>
